bd613d94d110f9f551cec70e7376401632136597
Francis Chung <francis.c.chung@gmail.com>
2020-01-07 19:00:08 +1100

    New Greeting Service implemtation for client

--- Client/Program.cs
+++ Client/Program.cs
@@ -5,6 +5,8 @@ using System.Text;
 using System.Threading.Tasks;
 using Dummy;
 using Grpc.Core;
+using Greet;
+using System.Threading;
 
 namespace Client
 {
@@ -13,6 +15,7 @@ namespace Client
         private const string Target = "127.0.0.1:50051";
         static void Main(string[] args)
         {
+            Thread.Sleep(2000);
             Channel channel = new Channel(Target, ChannelCredentials.Insecure);
 
             channel.ConnectAsync().ContinueWith((task) =>
@@ -23,7 +26,23 @@ namespace Client
                     Console.WriteLine($"Client Task Status: {task.ToString()}");
             });
 
-            var client = new DummyService.DummyServiceClient(channel);
+            //var client = new DummyService.DummyServiceClient(channel);
+
+            var client = new GreetingService.GreetingServiceClient(channel);
+            
+            var greeting = new Greeting()
+            {
+                FirstName = "Francis",
+                LastName = "Chung"
+            };
+
+            var request = new GreetingRequest()
+            {
+                Greeting = greeting
+            };
+
+            var response = client.Greet(request);
+            Console.WriteLine($"Response : {response.Result}");
 
             channel.ShutdownAsync().Wait();
             Console.ReadKey();
