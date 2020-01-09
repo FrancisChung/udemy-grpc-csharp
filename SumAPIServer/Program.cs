b81554bf8da4fbb27e87d466ce451820aa450435
Francis Chung <francis.c.chung@gmail.com>
2020-01-09 16:30:40 +1100

    SumAPIServer Code

---
+++ SumAPIServer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SumAPIServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ SumAPIServer/SumServiceImpl.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Grpc.Core;
+using SumAPI;
+using static SumAPI.SumService;
+
+namespace SumAPIServer
+{
+    class SumServiceImpl : SumServiceBase
+    {
+        public override Task<SumResponse> Sum(SumRequest request, ServerCallContext context)
+        {
+            var result = request.Number1 + request.Number2;
+            var sumResponse = new SumResponse()
+            {
+                Result = result
+            };
+
+            return Task.FromResult(sumResponse);
+            //return base.Sum(request, context);
+        }
+    }
+}
