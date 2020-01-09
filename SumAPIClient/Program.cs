ca2ff0e843958c030b19e43f6841a8ff106d5381
Francis Chung <francis.c.chung@gmail.com>
2020-01-09 20:04:14 +1100

    New Server Streaming Folder

--- SumAPIClient/Program.cs
+++ SumAPIClient/Program.cs
@@ -3,6 +3,7 @@ using Grpc.Core;
 using SumAPI;
 using System.Threading;
 using Tasks = System.Threading.Tasks;
+using System.IO;
 
 namespace SumAPIClient
 {
@@ -12,11 +13,13 @@ namespace SumAPIClient
 
         static void Main(string[] args)
         {
-            Thread.Sleep(1000);
-
-            Channel channel = new Channel($"localhost:{_port}", ChannelCredentials.Insecure);
-            channel.ConnectAsync().ContinueWith((task) =>
+            try
             {
+                Thread.Sleep(1000);
+
+                Channel channel = new Channel($"localhost:{_port}", ChannelCredentials.Insecure);
+                channel.ConnectAsync().ContinueWith((task) =>
+                {
                 //var taskResult = (task.Status == Tasks.TaskStatus.RanToCompletion ?
                 //                                "Client Ran Successfully" :
                 //                                $"TaskStatus : {task.ToString()}");
@@ -24,27 +27,29 @@ namespace SumAPIClient
                 //Console.WriteLine(taskResult);
 
                 if (task.Status == Tasks.TaskStatus.RanToCompletion)
-                    Console.WriteLine("The Client connected successfully");
-                else
-                    Console.WriteLine($"Client Task Status: {task.ToString()}");
-            });
+                        Console.WriteLine("The Client connected successfully");
+                    else
+                        Console.WriteLine($"Client Task Status: {task.ToString()}");
+                });
 
-            var client = new SumService.SumServiceClient(channel);
+                var client = new SumService.SumServiceClient(channel);
 
-            var request = new SumRequest()
-            {
-                Number1 = 10,
-                Number2 = 3
-            };
+                var request = new SumRequest()
+                {
+                    Number1 = 10,
+                    Number2 = 3
+                };
 
-            Console.WriteLine($"Client sending {request.Number1}, {request.Number2}");
+                Console.WriteLine($"Client sending {request.Number1}, {request.Number2}");
 
 
-            var response = client.Sum(request);
+                var response = client.Sum(request);
 
-            Console.WriteLine($"Result: {response.Result}");
+                Console.WriteLine($"Result: {response.Result}");
 
-            Console.ReadKey();
+                Console.ReadKey();
+            }
+            catch (IOException ex)
         }
     }
 }
