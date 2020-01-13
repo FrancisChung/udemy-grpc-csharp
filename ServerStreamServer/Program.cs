543c0e28bdd6966425ac3574db9e1167568d73e5
Francis Chung <francis.c.chung@gmail.com>
2020-01-13 20:58:34 +1100

    Working Server Code

--- ServerStreamServer/Program.cs
+++ ServerStreamServer/Program.cs
@@ -1,12 +1,37 @@
 ﻿using System;
+using Greet;
+using Grpc.Core;
+using System.IO;
 
 namespace ServerStreamServer
 {
     class Program
     {
+        private const int _port = 50053;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Grpc.Core.Server server = null;
+            try
+            {
+                server = new Grpc.Core.Server()
+                {
+                    Services = { GreetingService.BindService(new GreetingServiceImpl()) },
+                    Ports = { new Grpc.Core.ServerPort("localhost", _port, ServerCredentials.Insecure) }
+                };
+                server.Start();
+                Console.WriteLine(($"The Server is listening on port : {_port}"));
+                Console.ReadKey();
+
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine($"Server Connection Error: {e.Message}");
+            }
+            finally
+            {
+                server?.ShutdownAsync().Wait();
+            }
         }
     }
+
 }
