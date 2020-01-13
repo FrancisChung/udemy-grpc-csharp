2edeb26ae74eb127a67df916c61b54a6bb6f3768
Francis Chung <francis.c.chung@gmail.com>
2020-01-13 20:54:31 +1100

    Working baseline ServerStreamClient !!

---
+++ ServerStreamClient/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ServerStreamClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
