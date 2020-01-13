a8175903a372eb162088db1415677f037dd9e0a7
Francis Chung <francis.c.chung@gmail.com>
2020-01-13 18:48:55 +1100

    Shell Server Project

---
+++ ServerStreamServer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ServerStreamServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
