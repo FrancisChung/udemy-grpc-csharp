7db045de3d07de23f9b4620fe477821de6860341
Francis Chung <francis.c.chung@gmail.com>
2020-01-14 15:14:33 +1100

    Prime Decomposition Implementation

---
+++ PrimeServer/PrimeServiceImpl.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Grpc.Core;
+using PrimeAPI;
+using static PrimeAPI.PrimeService;
+
+namespace PrimeServer
+{
+    class PrimeServiceImpl : PrimeServiceBase
+    {
+        public override async Task Decompose(PrimeRequest request, IServerStreamWriter<PrimeResponse> responseStream, ServerCallContext context)
+        {
+            Console.WriteLine($"Request received to decompose {request.Number}");
+
+            int k = 2;
+            int n = request.Number;
+            while (n>1)
+            {
+                if (n % k == 0)
+                {
+                    await responseStream.WriteAsync(new PrimeResponse() { Decomposition = k });
+                    n = n / k;
+                }
+                else
+                    k++;
+            }
+            //return base.Decompose(request, responseStream, context);
+        }
+    }
+}
--- PrimeServer/Program.cs
+++ PrimeServer/Program.cs
@@ -1,12 +1,17 @@
 ﻿using System;
-
+using Grpc.Core;
 namespace PrimeServer
 {
     class Program
     {
+        private const int _port = 50054;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var server = new Grpc.Core.Server()
+            {
+                Ports = new ServerPort("localhost",_port, ServerCredentials.Insecure),
+                Services = { new  }
+            }
         }
     }
 }
