dd8d1005aca40cc8e812dcf98238056e11acc08c
Francis Chung <francis.c.chung@gmail.com>
2020-01-18 12:28:07 +1100

    Server code to bootstrap GreetingServiceImpl

--- ClientStreamServer/Program.cs
+++ ClientStreamServer/Program.cs
@@ -1,12 +1,36 @@
-﻿using System;
+﻿using Greet;
+using Grpc.Core;
+using System;
+using System.IO;
 
 namespace ClientStreamServer
 {
     class Program
     {
+        const int _port = 50054;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Grpc.Core.Server server = null;
+            try
+            {
+                server = new Grpc.Core.Server()
+                {
+                    Services = { GreetingService.BindService(new GreetingServiceImpl()) },
+                    Ports = { new Grpc.Core.ServerPort("localhost", _port, ServerCredentials.Insecure) }
+                };
+                server.Start();
+                Console.WriteLine(($"The Server is listening on port : {_port}"));
+                Console.ReadKey();
+
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine($"Server Connection Error: {e.Message}");
+            }
+            finally
+            {
+                server?.ShutdownAsync().Wait();
+            }
         }
     }
 }
