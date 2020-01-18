f75e7095a9dfa244de81b7757a16bc5a35840284
Francis Chung <francis.c.chung@gmail.com>
2020-01-18 11:57:45 +1100

    Client Stream Client Scaffolding

---
+++ ClientStreamClient/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ClientStreamClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
