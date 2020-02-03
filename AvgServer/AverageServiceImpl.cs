b8a39fad1ab67acd98f30603b80da6d71ed7910d
Francis Chung <francis.c.chung@gmail.com>
2020-02-03 19:17:57 +1100

    Convert to Doubles first before calculation

--- AvgServer/AverageServiceImpl.cs
+++ AvgServer/AverageServiceImpl.cs
@@ -22,7 +22,7 @@ namespace AvgServer
             }
 
             return new AverageResponse() {
-                Average = (sum / count)
+                Average = (double)sum / (double)count
             };
         }
     }
