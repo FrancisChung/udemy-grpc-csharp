a89d1b0b71ec7aadf682c95196123f315551fb15
Francis Chung <francis.c.chung@gmail.com>
2020-02-03 20:55:47 +1100

    GreetEveryone Service Implemented

---
+++ BiDiStreamServer/GreetingServiceImpl.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Greet;
+using Grpc.Core;
+using static Greet.GreetingService;
+
+namespace BiDiStreamServer
+{
+    class GreetingServiceImpl : GreetingServiceBase
+    {
+        public override async Task GreetEveryone(IAsyncStreamReader<GreetEveryoneRequest> requestStream, IServerStreamWriter<GreetEveryoneResponse> responseStream, ServerCallContext context)
+        {
+            while (await requestStream.MoveNext())
+            {
+                var result = $"Hello {requestStream.Current.Greeting.FirstName}," +
+                    $" {requestStream.Current.Greeting.LastName}";
+
+                Console.WriteLine($"Received: {result}");
+                await responseStream.WriteAsync(new GreetEveryoneResponse() { Result = result });
+            }
+        }
+    }
+}
