7ce2c468040aa0f95129ddc9109fee80a747ba95
Francis Chung <francis.c.chung@gmail.com>
2020-02-03 17:46:11 +1100

    Computer Average Client basic scaffolding

---
+++ AvgClient/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading;
+
+namespace AvgClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var target = "localhost:50055";
+            Thread.Sleep(1000);
+        }
+    }
+}
