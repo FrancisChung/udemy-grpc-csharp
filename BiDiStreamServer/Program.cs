4ee9443f47b116820ce68df483da3f1c80855ff4
Francis Chung <francis.c.chung@gmail.com>
2020-02-04 09:32:54 +1100

    Server Bootstrap code

--- BiDiStreamServer/Program.cs
+++ BiDiStreamServer/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Threading;
+using System.IO;
+using Greet;
+using Grpc.Core;
 
 namespace BiDiStreamServer
 {
@@ -6,7 +10,29 @@ namespace BiDiStreamServer
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            const int _port = 50055;
+
+            Grpc.Core.Server server = null;
+            try
+            {
+                server = new Grpc.Core.Server()
+                {
+                    Services = { GreetingService.BindService(new GreetingServiceImpl()) },
+                    Ports = { new ServerPort("localhost", _port, ServerCredentials.Insecure) }
+                };
+                server.Start();
+                Console.WriteLine(($"The Server is listening on port : {_port}"));
+                Console.ReadKey();
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine($"Server Connection Error: {e.Message}");
+            }
+            finally
+            {
+                server?.ShutdownAsync().Wait();
+            }
+
         }
     }
 }
