5d7e284b2ec1b4acf4e57b2073b8707cb6cddd20
Francis Chung <francis.c.chung@gmail.com>
2020-02-05 09:26:00 +1100

    Sqrt Server implementation

--- SqrtServer/Program.cs
+++ SqrtServer/Program.cs
@@ -1,12 +1,23 @@
 ﻿using System;
+using Grpc.Core;
 
 namespace SqrtServer
 {
     class Program
     {
+        const int _port = 50058;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+            Server server = new Server(){
+                Services = { Sqrt.SqrtService.BindService(new SqrtServiceImpl()) },
+                Ports = { new ServerPort("localhost", _port, ServerCredentials.Insecure) }
+            };
+
+            server.Start();
+
+            Console.WriteLine($"Sqrt Service started at port {_port}");
+            Console.ReadKey();
         }
     }
 }
