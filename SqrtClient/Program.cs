70ee74da64cefee1e60b8198ab6b3d2530298264
Francis Chung <francis.c.chung@gmail.com>
2020-02-05 08:58:48 +1100

    Sqrt Client/Server basics scaffolding

---
+++ SqrtClient/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SqrtClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
