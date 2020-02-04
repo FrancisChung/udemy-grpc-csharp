813a1135122534aa77b4e1179dd4bd4c2aaca53a
Francis Chung <francis.c.chung@gmail.com>
2020-02-05 09:08:41 +1100

    SqrtService Implementation

---
+++ SqrtServer/SqrtServiceImpl.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Grpc.Core;
+using Sqrt;
+using static Sqrt.SqrtService;
+
+namespace SqrtServer
+{
+    class SqrtServiceImpl :SqrtServiceBase   
+    {
+        public override async  Task<SqrtResponse> Sqrt(SqrtRequest request, ServerCallContext context)
+        {
+            var num = request.Number;
+
+            if (num >= 0)
+                return new SqrtResponse() { SquareRoot = Math.Sqrt(num) };
+            else
+                throw new RpcException(new Status(StatusCode.InvalidArgument, $"number : {num}"));
+        }
+    }
+}
