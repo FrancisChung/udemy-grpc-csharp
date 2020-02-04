f1723eccee41f4dcf669989213daa7f22fbbbe49
Francis Chung <francis.c.chung@gmail.com>
2020-02-04 11:14:25 +1100

    BiDirectional Client for GreetingEveryone

--- BiDiStreamClient/Program.cs
+++ BiDiStreamClient/Program.cs
@@ -1,12 +1,68 @@
-﻿using System;
+﻿using Greet;
+using Grpc.Core;
+using System;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace BiDiStreamClient
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var target = "localhost:50056";
+            Thread.Sleep(1000);
+
+            Channel channel = new Channel(target, ChannelCredentials.Insecure);
+            await channel.ConnectAsync().ContinueWith((task) =>
+            {
+                if (task.Status == TaskStatus.RanToCompletion)
+                    Console.WriteLine("The Client connected successfully");
+                else
+                    Console.WriteLine($"Client Task Status: {task.ToString()}");
+
+            });
+
+            var client = new Greet.GreetingService.GreetingServiceClient(channel);
+
+            await GreetEveryone(client);
+
+            channel.ShutdownAsync().Wait();
+            Console.ReadKey();
+        }
+
+        public static async Task GreetEveryone(Greet.GreetingService.GreetingServiceClient client)
+        {
+            var stream = client.GreetEveryone();
+
+            Greeting[] greetings =
+            {
+                new Greeting() {FirstName = "John", LastName ="Smith"},
+                new Greeting() {FirstName = "Pete", LastName = "Hansen"},
+                new Greeting() {FirstName = "Jane", LastName = "Doe"}
+            };
+
+            foreach (var g in greetings)
+            {
+                var request = new GreetEveryoneRequest() { Greeting = g };
+                await stream.RequestStream.WriteAsync(request);
+            }
+            await stream.RequestStream.CompleteAsync();
+
+            var responseTask = Task.Run(async () =>
+            {
+                while (await stream.ResponseStream.MoveNext())
+                {
+                    var c = stream.ResponseStream.Current;
+                    Console.WriteLine($"Response is: {c.Result}");
+                }
+            });
+
+            await responseTask;
+
+            
         }
     }
 }
