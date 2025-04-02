eb43c79b960927d1a80fa36f1714b3809972f75c
Francis Chung <francis.c.chung@gmail.com>
2025-04-02 15:35:37 +1100

    Initial version of MaximumClient Solution - WIP

---
+++ FindMaximumServer/FindMaxServiceImpl.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static FindMaximum.FindMaxService;
+
+namespace FindMaximumServer
+{ 
+    internal class FindMaxServiceImpl : FindMaxServiceBase
+    {
+        private int? max = null;
+        while (await requestStream.MoveNext())
+        {
+            if (!max.HasValue ||  max <requestStream.Current.Number>>)
+            {
+                max = requestStream.Current.Number;
+                await responseStream.WriteAsync(new FindMaxResponse() { Max = max.Value });
+            }
+        }
+    }
+}
---
+++ FindMaximumServer/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading;
+using System.IO;
+using FindMaximumServer;
+using Grpc.Core;
+
+namespace FindMaximumServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int _port = 50056;
+
+            Grpc.Core.Server server = null;
+            try
+            {
+                server = new Grpc.Core.Server()
+                {
+                    Services = { FindMaximumService.BindService(new FindMaximumServiceImpl()) },
+                    Ports = { new ServerPort("localhost", _port, ServerCredentials.Insecure) }
+                };
+                server.Start();
+                Console.WriteLine(($"The Server is listening on port : {_port}"));
+                Console.ReadKey();
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine($"Server Connection Error: {e.Message}");
+            }
+            finally
+            {
+                server?.ShutdownAsync().Wait();
+            }
+
+        }
+    }
+}
