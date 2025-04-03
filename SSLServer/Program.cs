bb3facbead4d15d2b36717fbd0a42ecd2e5e2c00
Francis Chung <francis.c.chung@gmail.com>
2025-04-03 15:12:12 +1100

    Server code added (From other Server repo)

--- SSLServer/Program.cs
+++ SSLServer/Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Greet;
+using Grpc.Core;
+
+namespace Server
+{
+    class Program
+    {
+        private const int Port = 50051;
+        static void Main(string[] args)
+        {
+            Grpc.Core.Server server = null;
+
+            try
+            {
+                server = new Grpc.Core.Server()
+                {
+                    Services = { GreetingService.BindService(new GreetingServiceImpl()) },
+                    Ports = { new ServerPort("localhost", Port, ServerCredentials.Insecure) }
+                };
+                server.Start();
+                Console.WriteLine(($"The Server is listening on port : {Port}"));
+                Console.ReadKey();
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine($"The server failed to start: {e.Message}");
+                throw;
+            }
+            finally
+            {
+                server?.ShutdownAsync().Wait();
+
+                //if (server != null)
+                //    server.ShutdownAsync().Wait();
+            }
+
+        }
+    }
+}
