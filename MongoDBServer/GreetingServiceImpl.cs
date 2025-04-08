6666259c50eef08fae1709d9a702fcef25f2d760
Francis Chung <francis.c.chung@gmail.com>
2025-04-08 17:45:24 +1000

    Namespace changes + GreetingServiceImpl added

---
+++ MongoDBServer/GreetingServiceImpl.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Greet;
+using Grpc.Core;
+using static Greet.GreetingService;
+
+namespace MongoDBServer
+{
+    public class GreetingServiceImpl : GreetingServiceBase
+    {
+        public override Task<GreetingResponse> Greet(GreetingRequest request, ServerCallContext context)
+        {
+            string result = $"Hello {request.Greeting.FirstName} {request.Greeting.LastName}";
+            return Task.FromResult(new GreetingResponse() { Result = result });
+            //return base.Greet(request, context);
+        }
+    }
+}
--- MongoDBServer/Program.cs
+++ MongoDBServer/Program.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 using Greet;
 using Grpc.Core;
 
-namespace Server
+namespace MongoDBServer
 {
     class Program
     {
