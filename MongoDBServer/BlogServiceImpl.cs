5767fa2285acd17ed48b10d90389613e205d6d6c
Francis Chung <francis.c.chung@gmail.com>
2025-04-08 18:47:26 +1000

    Skeletion BlogServiceImpl

---
+++ MongoDBServer/BlogServiceImpl.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Blog.BlogService;
+
+namespace MongoDBServer
+{
+    internal class BlogServiceImpl : BlogServiceBase
+    {
+    }
+}
